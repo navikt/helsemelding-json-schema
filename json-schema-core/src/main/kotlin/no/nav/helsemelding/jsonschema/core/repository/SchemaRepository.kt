346415c46871b9f4ba59433ff518d753d68df0eb
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-06-05 10:11:59 +0200

    Cleanups

--- json-schema-core/src/main/kotlin/no/nav/helsemelding/jsonschema/core/repository/SchemaRepository.kt
+++ json-schema-core/src/main/kotlin/no/nav/helsemelding/jsonschema/core/repository/SchemaRepository.kt
@@ -1,8 +1,9 @@
 package no.nav.helsemelding.jsonschema.core.repository
 
 import arrow.core.Either
-import arrow.core.flatMap
 import arrow.core.memoize
+import arrow.core.raise.catch
+import arrow.core.raise.either
 import io.github.optimumcode.json.schema.JsonSchema
 import no.nav.helsemelding.jsonschema.core.error.SchemaError
 import no.nav.helsemelding.jsonschema.core.model.SchemaType
@@ -19,23 +20,27 @@ class JsonSchemaRepository(
     override fun get(
         messageType: SchemaType,
         version: Int
-    ): Either<SchemaError, JsonSchema> =
-        schema(messageType, version)
+    ): Either<SchemaError, JsonSchema> = schema(messageType, version)
 
     private fun loadSchema(
-        messageType: SchemaType,
+        schemaType: SchemaType,
         version: Int
-    ): Either<SchemaError, JsonSchema> =
-        schemaDocumentRepository
-            .get(messageType, version)
-            .flatMap { document ->
-                Either.catch { JsonSchema.fromDefinition(document.content) }
-                    .mapLeft { throwable ->
-                        SchemaError.InvalidSchema(
-                            schemaType = messageType,
-                            version = version,
-                            reason = throwable.message ?: "Failed to load schema"
-                        )
-                    }
+    ): Either<SchemaError, JsonSchema> = either {
+        val document = schemaDocumentRepository
+            .get(schemaType, version)
+            .bind()
+
+        catch(
+            { JsonSchema.fromDefinition(document.content) },
+            { t ->
+                raise(
+                    SchemaError.InvalidSchema(
+                        schemaType = schemaType,
+                        version = version,
+                        reason = t.message ?: "Failed to load schema"
+                    )
+                )
             }
+        )
+    }
 }
