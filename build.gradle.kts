4e55f088c1d9c175d627ff9f9fd20204aceb72b1
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-06-26 09:06:41 +0200

    Upgrade dependencies to latest version (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    kotlin("jvm") version "2.2.0" apply false
+    kotlin("jvm") version "2.4.0" apply false
     kotlin("plugin.serialization") version "2.1.10" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
     id("com.gradleup.shadow") version "8.3.6" apply false
-    id("io.ktor.plugin") version "3.5.0" apply false
+    id("io.ktor.plugin") version "3.5.1" apply false
 }
 
 subprojects {
