c117bc95e79ec3705235a729de8f6de322475e93
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2026-07-21 08:28:21 +0200

    Added forced sorting order

--- json-schema-core/src/test/kotlin/no/nav/helsemelding/jsonschema/core/repository/SchemaDocumentRepositorySpec.kt
+++ json-schema-core/src/test/kotlin/no/nav/helsemelding/jsonschema/core/repository/SchemaDocumentRepositorySpec.kt
@@ -19,7 +19,7 @@ class SchemaDocumentRepositorySpec : StringSpec(
             documents shouldHaveSize 2
 
             documents shouldContain SchemaDocument(
-                schemaType = SchemaType.OUTGOING_DIALOG_MESSAGE,
+                schemaType = SchemaType.INCOMING_DIALOG_MESSAGE,
                 version = 1,
                 schema = documents.first().schema
             )
