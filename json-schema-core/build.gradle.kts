c822eabdfc567400e0b11a7b67e4ef73a27c13de
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-09-15 16:04:38 +0200

    Oppdatert bibliotekets versjon

--- json-schema-core/build.gradle.kts
+++ json-schema-core/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.helsemelding"
             artifactId = "json-schema-core"
-            version = "0.0.4"
+            version = "0.0.5"
             from(components["java"])
         }
     }
